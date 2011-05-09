cc8bcebcae9ec7005905c1c8edea391b4ba3b60a
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-05-09 16:00:49 -0400

    Updated

--- source/nothinbutdotnetprep/collections/MovieLibrary.cs
+++ source/nothinbutdotnetprep/collections/MovieLibrary.cs
@@ -18,7 +18,6 @@ namespace nothinbutdotnetprep.collections
             return movies.one_at_a_time();
         }
 
-
         public void add(Movie movie)
         {
             if (already_contains(movie)) return;
@@ -35,109 +34,49 @@ namespace nothinbutdotnetprep.collections
             throw new NotImplementedException();
         }
 
-        private IList<Movie> all_movies_by_criteria(Criteria criteria)
+        IEnumerable<Movie> all_matching(MovieCriteria matches)
         {
-            IList<Movie> movies = new List<Movie>();
             foreach (var movie in movies)
             {
-                switch (criteria.CriteriaType)
-                {
-                    case Criteria.CriteriaTypes.ByGenre:
-                        if (movie.production_studio==criteria.Studio)
-                            movies.Add(movie);
-                        break;
-                    case Criteria.CriteriaTypes.ByYearRange:
-                        if (movie.date_published.Year>=criteria.DateFrom & movie.date_published.Year<=criteria.DateTo)
-                            movies.Add(movie);
-                        break;
-                    case Criteria.CriteriaTypes.AfterYear:
-                        if (movie.date_published.Year > criteria.Year)
-                            movies.Add(movie);
-                        break;
-                }             
+                if (matches(movie)) yield return movie;
             }
-            return movies;
-
         }
 
         public IEnumerable<Movie> all_movies_published_by_pixar()
         {
-            return all_movies_by_criteria( new Criteria().ByProductionStudio(ProductionStudio.Pixar));
+            return all_matching(movie => movie.production_studio == ProductionStudio.Pixar);
         }
 
         public IEnumerable<Movie> all_movies_published_after(int year)
         {
-            return all_movies_by_criteria(new Criteria().AfterYear(year));
+            return all_matching(movie => movie.date_published.Year > year);
         }
 
         public IEnumerable<Movie> all_movies_published_between_years(int startingYear, int endingYear)
         {
-            IList<Movie> betweenMovies = new List<Movie>();
-            foreach (Movie movie in movies)
-            {
-                if (movie.date_published.Year >= startingYear && movie.date_published.Year<=endingYear)
-                {
-                    betweenMovies.Add(movie);
-                }
-            }
-
-            return betweenMovies;
+            return all_matching(movie => movie.date_published.Year >= startingYear &&
+                movie.date_published.Year <= endingYear);
         }
 
         public IEnumerable<Movie> all_movies_published_by_pixar_or_disney()
         {
-            IList<Movie> pixarDisneyMovies = new List<Movie>();
-            foreach (Movie movie in movies)
-            {
-                if (movie.production_studio == ProductionStudio.Pixar || movie.production_studio==ProductionStudio.Disney)
-                {
-                    pixarDisneyMovies.Add(movie);
-                }
-            }
-
-            return pixarDisneyMovies;
+            return all_matching(movie => movie.production_studio == ProductionStudio.Disney ||
+                movie.production_studio == ProductionStudio.Pixar);
         }
 
         public IEnumerable<Movie> all_kid_movies()
         {
-            IList<Movie> kidMovies = new List<Movie>();
-            foreach (Movie movie in movies)
-            {
-                if (movie.genre==Genre.kids)
-                {
-                    kidMovies.Add(movie);
-                }
-            }
-
-            return kidMovies;
+            return all_matching(movie => movie.genre == Genre.kids);
         }
 
         public IEnumerable<Movie> all_action_movies()
         {
-            IList<Movie> actionMovies = new List<Movie>();
-            foreach (Movie movie in movies)
-            {
-                if (movie.genre == Genre.action)
-                {
-                    actionMovies.Add(movie);
-                }
-            }
-
-            return actionMovies;
+            return all_matching(movie => movie.genre == Genre.action);
         }
 
         public IEnumerable<Movie> all_movies_not_published_by_pixar()
         {
-            IList<Movie> notpixarMovies = new List<Movie>();
-            foreach (Movie movie in movies)
-            {
-                if (movie.production_studio != ProductionStudio.Pixar)
-                {
-                    notpixarMovies.Add(movie);
-                }
-            }
-
-            return notpixarMovies;
+            return all_matching(movie => movie.production_studio != ProductionStudio.Pixar);
         }
 
         public IEnumerable<Movie> sort_all_movies_by_title_ascending
@@ -160,55 +99,4 @@ namespace nothinbutdotnetprep.collections
             throw new NotImplementedException();
         }
     }
-
-    public class Criteria
-    {
-        public enum CriteriaTypes
-        {
-            ByGenre,
-            ByYearRange,
-            AfterYear,
-            ByTitle,
-            ByStudio
-        }
-
-        public CriteriaTypes CriteriaType { get; private set; }
-        public Genre Genre { get; private set; }
-        public int DateFrom { get; private set; }
-        public int DateTo { get; private set; }
-        public int Year { get; private set; }
-        public ProductionStudio Studio { get; set; }
-
-        public Criteria ByGenre(Genre genre)
-        {
-            CriteriaType = CriteriaTypes.ByGenre;
-            Genre = genre;
-            return this;
-        }
-
-
-        public Criteria ByDateRange(int from, int to)
-        {
-            CriteriaType = CriteriaTypes.ByYearRange;
-            DateFrom = from;
-            DateTo = to;
-            return this;
-        }
-
-        public Criteria ByProductionStudio(ProductionStudio studio)
-        {
-            CriteriaType = CriteriaTypes.ByStudio;
-            Studio = studio;
-            return this;
-        }
-
-        public Criteria AfterYear(int year)
-        {
-            CriteriaType = CriteriaTypes.AfterYear;
-            Year = year;
-            return this;
-
-        }
-    }
-
 }
