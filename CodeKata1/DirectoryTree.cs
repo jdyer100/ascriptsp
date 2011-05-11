a0d1a31974208b719823daeda887172b265f1e7c
Jeff Dyer <jeffdyer100@email.com>
2011-05-11 09:24:41 -0400

    Committing

---
+++ CodeKata1/DirectoryTree.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CodeKata1
+{
+    public class DirectoryTree
+    {
+        private string indentation = @" ";
+        public DirectoryTree(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        public string FilePath { get; set; }
+
+        public void PrintTree()
+        {
+            DisplayFileName(FilePath);
+        }
+
+        private void DisplayFileName(string directoryPath)
+        {
+            foreach (var fileName in Directory.EnumerateFiles(directoryPath))
+            {
+                Console.WriteLine(indentation + fileName);
+            }
+        }
+    }
+}
---
+++ CodeKata1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeKata1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryTree directoryTree = new DirectoryTree(@"c:\nbdn\prep");
+            directoryTree.PrintTree();
+            Console.Read();
+        }
+    }
+}
